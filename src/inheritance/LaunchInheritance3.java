1cd5d5e1d9706cc983e05e160faf127b1924a14c
Nuzhat <nuzhat.mujahid@gmail.com>
2025-08-23 12:54:58 +0530

    Update LaunchInheritance3.java
    New Message from github

--- src/inheritance/LaunchInheritance3.java
+++ src/inheritance/LaunchInheritance3.java
@@ -3,5 +3,6 @@ package inheritance;
 public class LaunchInheritance3 {
     public static void main(String[] args) {
         System.out.println("New class added");
+         System.out.println("New Message from github");
     }
 }
