9f3bc5d97e453718496af43f9a28e50fdb22167d
NuzhatJamadar <nuzhat.mujahid@gmail.com>
2025-08-23 13:10:42 +0530

    Eclipse

--- src/inheritance/LaunchInheritance3.java
+++ src/inheritance/LaunchInheritance3.java
@@ -4,5 +4,7 @@ public class LaunchInheritance3 {
     public static void main(String[] args) {
         System.out.println("New class added");
          System.out.println("New Message from github");
+         System.out.println("New Message from Eclipse");
+
     }
 }
